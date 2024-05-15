4482856cbefbd3ec32871237df6fc015469b7370
time <time@netlight.com>
2024-05-15 11:50:53 +0200

    Add comments to UserContext functions

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -2,15 +2,36 @@ import { createContext, useContext, useEffect, useState } from 'react'
 import { getUsers } from '../api/apiService'
 import { User } from '../models/User'
 
+/**
+ *
+ * This file is for Exercise 2
+ *
+ */
+
+/**
+ * Defines what the context will contain
+ *
+ * users is an array of {@link User} objects
+ */
 interface IUserContext {
   isLoading: boolean
   users: User[] | null
 }
 
+/**
+ * Define and create a new {@link React.Context} object, typed to our custom interface
+ * {@link IUserContext} or undefined.
+ */
+
 export const UserContext: React.Context<IUserContext | undefined> = createContext<
   IUserContext | undefined
 >(undefined)
 
+/**
+ * Custom hook to get the current context value
+ *
+ * @returns {@link UserContext}
+ */
 export const useUsers = (): IUserContext => {
   const context = useContext(UserContext)
   if (!context) {
@@ -19,6 +40,11 @@ export const useUsers = (): IUserContext => {
   return context
 }
 
+/**
+ *
+ * @param children The component the Context should be provided to by wrapping it
+ * @returns {@link UserContext.Provider}
+ */
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [users, setData] = useState<User[] | null>(null)
   const [isLoading, setIsLoading] = useState(false)
